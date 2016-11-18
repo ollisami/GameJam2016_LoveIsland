46eeb1952694ff7bd19f7f10fd844c3cac62e924
Janne Pulkkinen <jannepulk@gmail.com>
2016-11-19 00:23:07 +0200

    Add palm trees randomly around the island

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+	private int DECORATION_COUNT = 5;
+
+	GameObject gameObject;
+
+	public Transform palmTree;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < DECORATION_COUNT; i++) {
+			float x = Random.Range (-4, 4);
+			float y = Random.Range (-4, 4);
+
+			Instantiate (palmTree, new Vector3 (x, y, 0), Quaternion.identity);
+			print (x + "x" + y);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
