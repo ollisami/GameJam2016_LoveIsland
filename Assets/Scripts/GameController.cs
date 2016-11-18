7e2ae9b9edcc1aeacf0b4e04067846840393bb36
ollisami <ollisami@cs.helsinki.fi>
2016-11-18 19:33:55 +0200

    create project

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	private int mapSize_X = 10;
+	private int mapSize_Y = 10;
+
+
+	public GameObject[] peoplePrefabs;
+	// Use this for initialization
+	void Start () {
+		spawnPeople (5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void spawnPeople(int count) {
+		for (int i = 0; i < count; i++) {
+			Vector2 pos = getRandomPos ();
+			Instantiate (peoplePrefabs[Random.Range(0, peoplePrefabs.Length)], pos, Quaternion.identity);
+		}
+	}
+
+
+	private Vector2 getRandomPos() {
+		return new Vector2(Random.Range((mapSize_X/2)*-1, mapSize_X/2), Random.Range((mapSize_Y/2)*-1, mapSize_Y/2));
+	}
+
+}
---
+++ Assets/Scripts/peopleMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class peopleMovement : MonoBehaviour {
+
+	private int mapSize_X;
+	private int mapSize_Y;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
