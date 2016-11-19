ae8ed4e74ae817d76402dad20c590da46a4aa090
Aino Lipsanen <Aino Lipsanen>
2016-11-19 14:26:43 +0200

    PersonBehavior tweaks

--- Assets/Scripts/PersonBehavior.cs
+++ Assets/Scripts/PersonBehavior.cs
@@ -6,14 +6,20 @@ public class PersonBehavior : MonoBehaviour {
 	float speed;
 	float size;
 
+	public float MinSize = 0.75f;
+	public float MaxSize = 1.25f;
+
+	public float MinSpeed = 0.7f;
+	public float MaxSpeed = 1.3f;
+
 	// Use this for initialization
 	void Start () {
 		// Give person a randomized set of features
 		// eg. speed, size, infection length...
 
 		// speed and size are multipliers
-		speed = Random.Range(0.6f, 1.5f);
-		size = Random.Range (0.60f, 1.25f);
+		speed = Random.Range(this.MinSpeed, this.MaxSpeed);
+		size = Random.Range (this.MinSize, this.MaxSpeed);
 
 		transform.localScale = new Vector3 (size, size, 0);
 	}
