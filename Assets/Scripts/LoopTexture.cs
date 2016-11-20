cd7b951aa31d7b7fc4e0036ffc6e3b9bc63d6fa7
ollisami <ollisami@cs.helsinki.fi>
2016-11-20 16:09:42 +0200

    bg loop fix

--- Assets/Scripts/LoopTexture.cs
+++ Assets/Scripts/LoopTexture.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 
 public class LoopTexture : MonoBehaviour {
-		public float scrollSpeed = 0.5F;
+		private float scrollSpeed = 0.2F;
 		public Renderer rend;
+	private float offset = 0;
 		void Start() {
 			rend = GetComponent<Renderer>();
 		}
 		void Update() {
-			float offset = Time.time * scrollSpeed;
-			rend.material.mainTextureOffset = new Vector2(offset, 0);
+		if (offset > 0.1F || offset < -0.1F)
+			scrollSpeed = -scrollSpeed;	
+		offset += Time.deltaTime * scrollSpeed;
+		rend.material.mainTextureOffset = new Vector2(offset, 0);
 
 		}
 	}
