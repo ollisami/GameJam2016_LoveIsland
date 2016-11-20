0f210e5c830eb9fafab4d7710588e3f13f334bda
JaakkoLipsanen <jaakko.lipsanen@outlook.com>
2016-11-20 16:40:21 +0200

    Adjusted background sea texture looping to be smoother

--- Assets/Scripts/LoopTexture.cs
+++ Assets/Scripts/LoopTexture.cs
@@ -2,17 +2,29 @@
 using System.Collections;
 
 public class LoopTexture : MonoBehaviour {
-		private float scrollSpeed = 0.2F;
 		public Renderer rend;
-	private float offset = 0;
+		private float offset2 = 0;
+		private int dir = 1;
 		void Start() {
 			rend = GetComponent<Renderer>();
 		}
 		void Update() {
-		if (offset > 0.1F || offset < -0.1F)
-			scrollSpeed = -scrollSpeed;	
-		offset += Time.deltaTime * scrollSpeed;
-		rend.material.mainTextureOffset = new Vector2(offset, 0);
+		if (dir == 1) {
+			offset2 += Time.deltaTime * 0.5f;
+			if (offset2 > 1) {
+				offset2 = 1 - (offset2 - 1);
+				dir = -1;
+			}
+		} else {
+			offset2 -= Time.deltaTime * 0.5f;
+			if (offset2 < 0) {
+				offset2 = -offset2;
+				dir = 1;
+			}
+		}
 
+
+
+			rend.material.mainTextureOffset = new Vector2(Mathf.SmoothStep(-0.15f, 0.15f, offset2), 0);
 		}
 	}
